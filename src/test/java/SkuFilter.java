dc0bf456146eababbd710c114406758432ee0547
Rudyroom1 <rudy.crypt@gmail.com>
2019-03-27 11:32:24 +0200

    Added some changes.

--- src/test/java/SkuFilter.java
+++ src/test/java/SkuFilter.java
@@ -5,6 +5,7 @@ import skuJsonObject.TableObject;
 import urlWork.SkuUrl;
 import workWithFile.FileWork;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +14,13 @@ public class SkuFilter {
     public static void main(String[] args) {
         SkuUrl url = new SkuUrl();
         TableObject tableObject = new TableObject();
-        ArrayList<String> table = FileWork.readingFile("C:\\Users\\Yaros\\IdeaProjects\\ScuFilter\\sku_in_store_is_deliverable1.table");
+        String pathToFile = ".\\src\\main\\resources\\DataFiles\\sku_in_store_is_deliverable1.table";
+        ArrayList<String> table = FileWork.readingFile(pathToFile);
         List<String> list = FileWork.getFilteredData(table, "IsOfSourceFilter$");
         List<String> parsedList = url.getListOfSkuWithLenght(list, 50);
         List<String> urlList = url.getCreatedUrl(BasedUrls.BASE_URL_UAT, PathSegment.PRICE_AVAILABILITY, parsedList, UrlParams.BANNER_CTR, UrlParams.IS_KIOSK, UrlParams.LANGUAGE_ENGLISH, UrlParams.STORE_0987);
         List<TableObject> filteredList = url.getHttpRequest(urlList);
         String filteredData = tableObject.getFilteredListByQuantity(filteredList, "0");
-        FileWork.writeToFile(filteredData, "Filtered_sku.txt");
+        FileWork.writeToFile(filteredData, ".\\src\\main\\resources\\DataFiles\\Filtered_sku.txt");
     }
 }
